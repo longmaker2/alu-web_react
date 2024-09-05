c6b625ee10f821fbe9a386df5aea89bf2cde8fe9
longmaker2 <l.deng@alustudent.com>
2024-09-05 23:28:48 +0200

    updated

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -2,6 +2,8 @@
 import $ from "jquery";
 
 // add three different p tags to body
-$("body").append("<p>Holberton Dashboard</p>");
-$("body").append("<p>Dashboard data for the students</p>");
-$("body").append("<p>Copyright - Holberton School</p>");
+[
+  "$('body').append('<p>Holberton Dashboard</p>');",
+  "$('body').append('<p>Dashboard data for the students</p>');",
+  "$('body').append('<p>Copyright - Holberton School</p>');",
+];
